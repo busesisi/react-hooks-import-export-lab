b6e77817cb68db58ab69a0b5a8c1993927e8381e
busesisi <buse.senturk@outlook.com>
2021-07-19 20:57:49 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import {image} from "../data/user.js";
+import Home from "./Home.js";
+import NavBar from "./NavBar.js";
+import About from "./About.js";
 
 function App() {
   return (
